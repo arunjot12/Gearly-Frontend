fa89db3834a18329fa20edd1f71646bfb48a1c8f
Arunjot Singh <arunjot@antiersolutions.com>
2026-08-17 12:09:40 +0530

    chore: Updated the env

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://127.0.0.1:3000';
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://127.0.0.1:3000';
 
 const apiClient = axios.create({
   baseURL: BASE_URL,
